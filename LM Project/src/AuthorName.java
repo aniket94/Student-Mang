23edb22d5f01a4a5f445ea962902d5ee9668c0a5
aniket.kudtarkar <aniket.kudtarkar@B107-18.soit.local>
2016-07-25 09:59:41 +1200

    created the first 2 classes

---
+++ LM Project/src/AuthorName.java
@@ -0,0 +1,17 @@
+
+public class AuthorName 
+{
+
+	public static void main(String[] args) 
+	{
+		BooksInfo book1 = new BooksInfo("ABC", 123, "J.K.Rowling" );
+		book1.displayinfo();
+		
+		BooksInfo book2 = new BooksInfo("DEF", 231, "Dan Brown" );
+		book2.displayinfo();
+		
+		BooksInfo book3 = new BooksInfo("AFG", 231, "Dan Brown" );
+		book3.displayinfo();
+	}
+
+}   
